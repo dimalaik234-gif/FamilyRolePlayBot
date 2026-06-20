4cfe5ca46aaf3e843b0cc160261c069ae2cf3d61
dimalaik234-gif <dimanaumov234@mail.ru>
2026-06-20 11:51:02 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# Токен бота (вставить свой)
+8898239222:AAFyly_OY2zfqC-iTCZqhiC_SOC-WZgaK-A = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+
+# Путь к базе данных
+DB_PATH = "database.db"
