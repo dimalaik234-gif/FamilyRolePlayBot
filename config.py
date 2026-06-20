6d3c222d2e1669962f983f732673cd08aca9222f
dimalaik234-gif <dimanaumov234@mail.ru>
2026-06-20 12:01:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Токен бота (вставить свой)
-8898239222:AAFyly_OY2zfqC-iTCZqhiC_SOC-WZgaK-A = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8898239222:AAFyly_OY2zfqC-iTCZqhiC_SOC-WZgaK-A")
 
 # Путь к базе данных
 DB_PATH = "database.db"
